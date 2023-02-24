eddb5f689f83a7f36a535f7f30c329cbb4387612
groomyjohny <groomyjohny@gmail.com>
2023-02-24 22:11:51 +0500

    Added logging

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from datetime import datetime
 import os
+import sys
 
+class Logger:
+    prev_str = '\n'
+ 
+    def __init__(self, filename):
+        self.console = sys.stdout
+        self.file = open(filename, 'a')
+ 
+    def write(self, message):
+        datetime_str = f'[{datetime.now()}] '
+        if not (len(self.prev_str) > 0 and self.prev_str[-1] == '\n'):
+            datetime_str = ''
+        new_message = f'{datetime_str}{message}'
+        self.console.write(new_message)
+        self.file.write(new_message)
+        self.prev_str = message
+ 
+    def flush(self):
+        self.console.flush()
+        self.file.flush()
+		
+
+sys.stdout = Logger('log.txt')		
 repoPaths = None
+
 with open('repos.txt','r') as f:
     repoPaths = f.readlines()
-
 for _path in repoPaths:
     path = _path[:-1] if _path[-1] == '\n' else _path # strip newline
     print(f'Attempting to commit {path}')
